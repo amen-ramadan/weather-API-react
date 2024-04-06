d65429f54181df72aadd3c1a575823d4b03754fe
amen <ramadanamen407@gmail.com>
2024-04-06 22:16:14 +0300

    Async Logic with Redux Revision

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import { useTranslation } from 'react-i18next';
-import axios from "axios";
 import moment from "moment";
 
 // redux import
@@ -17,7 +16,8 @@ moment.locale("ar"); // تعيين اللغة العربية لـ moment.js
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { setWeather } from "./weatherApiSlice";
+import { fetchWeather } from "./weatherApiSlice";
+import { CircularProgress } from "@mui/material";
 
 const theme = createTheme({
   typography: {
@@ -29,18 +29,12 @@ let cancelAxios = null;
 function App() {
   
   const dispatch = useDispatch();
-  const result = useSelector( ( state ) => console.log(state.weather.result)  );
+  const isLoading = useSelector( ( state ) => state.weather.isLoading  );
+  const temp = useSelector( ( state ) => state.weather.weather );
 
   // states
   const { t, i18n } = useTranslation();
   const [dateAndTime, setDateAndTime] = useState("");
-  const [temp, setTemp] = useState({
-    number: null,
-    description: "",
-    min: null, 
-    max: null,
-    icon: "",
-  }); // for the all keys
   let direction = i18n.language === "ar"? "rtl": "ltr"
 
 
@@ -56,39 +50,14 @@ function App() {
   // language change
   useEffect( () =>
   {
-    dispatch(setWeather());
+    dispatch(fetchWeather())
     i18n.changeLanguage("ar");
   },[])
   // moment and api
   useEffect(() => {
     moment.locale("ar"); // تعيين اللغة العربية لـ moment.js
     setDateAndTime(moment().format('LLLL')); // تنسيق التاريخ والوقت باللغة العربية
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=35.56200&lon=45.31700&appid=24f7438ece98c2de5a5ce455b4f63b43",
-        {
-          cancelToken: new axios.CancelToken((c) => {
-            cancelAxios = c;
-          }),
-        }
-      )
-      .then((response) => {
-        const number = Math.round(response.data.main.temp - 273.15);
-        const min = Math.round(response.data.main.temp_min - 273.15);
-        const max = Math.round(response.data.main.temp_max - 273.15);
-        const description = response.data.weather[0].description;
-        const icon = response.data.weather[0].icon;
-        setTemp((prevTemp) => ({
-          ...prevTemp,
-          min: min,
-          max: max,
-          description: description,
-          number: number,
-          icon: icon
-        }));})
-      .catch((error) => {
-        console.log(new Error(error));
-      });
+    
 
       return () => {      // clean up
         console.log("clean up")
@@ -164,11 +133,11 @@ function App() {
                   <div>
                     {/* TEMP */}
                     <div>
+                      {isLoading && <CircularProgress style={{ color: "white" }}/>}
                       <Typography variant="h1" style={{ textAlign: "right" }}>
                         {temp.number}
                       </Typography>
 
-                      {/* <img src={`https://openweathermap.org/img/wn/${temp.icon}@4x.png`} alt="icon-weather" /> */}
                     </div>
                     {/*== TEMP ==*/}
 
@@ -189,13 +158,8 @@ function App() {
                   </div>
                   {/*== DEGREE & DESCRIPTION ==*/}
 
-                  <img src={`https://openweathermap.org/img/wn/${temp.icon}@4x.png`} alt="icon-weather" />
-                  {/* <CloudIcon
-                    style={{
-                      fontSize: "200px",
-                      color: "white",
-                    }}
-                  /> */}
+                  <img src={temp.icon} alt="icon-weather" />
+
                 </div>
                 {/*= CONTAINER OF DEGREE + CLOUD ICON ==*/}
               </div>
--- src/weatherApiSlice.js
+++ src/weatherApiSlice.js
@@ -1,14 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
+
+export const fetchWeather = createAsyncThunk( "weather/fetchWeather", async () =>
+{
+  const response = await axios.get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=35.56200&lon=45.31700&appid=24f7438ece98c2de5a5ce455b4f63b43",
+
+      )
+        const number = Math.round(response.data.main.temp - 273.15);
+        const min = Math.round(response.data.main.temp_min - 273.15);
+        const max = Math.round(response.data.main.temp_max - 273.15);
+        const description = response.data.weather[0].description;
+        const icon = response.data.weather[0].icon;
+
+        return { number, min, max, description, icon: `https://openweathermap.org/img/wn/${icon}@4x.png` };
+      
+})
 const weatherApiSlice = createSlice({
   name: "weatherApi",
   initialState: {
     result: "empty",
+    weather: {},
+    isLoading: false,
   },
   reducers: {
     setWeather: (state, action) => {
       state.result = "changed";
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWeather.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchWeather.fulfilled, (state, action) => {
+        state.result = "fulfilled";
+        state.weather = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(fetchWeather.rejected, (state, action) => {
+        state.isLoading = false;
+      });
   },
 } );
 
